557a03ed0f3a32e812b5bf99da5aad7daf9eb6a2
RAMESH PANDEY <pandeyramesh400@gmail.com>
2022-08-10 00:04:33 +0530

    Declared Local Variable LCM and HCF in optimised LCM

--- src/com/PracticeJavaWithPandey/MathsProblem/LCM/OptimisedLCM.java
+++ src/com/PracticeJavaWithPandey/MathsProblem/LCM/OptimisedLCM.java
@@ -3,8 +3,7 @@ package com.PracticeJavaWithPandey.MathsProblem.LCM;
 import java.util.Scanner;
 
 public class OptimisedLCM {
-        int LCM = 1 ;
-        int HCF=1;
+
         //BAsic appproach to get HCF or GCD of two number
 //Example  : 10 & 20......... 10=10,20,30,40,50, & 20= 20,40,60,80 lcm is 20
 
@@ -16,16 +15,18 @@ public class OptimisedLCM {
             return getOptimisedHcf(val2,val1%val2);
         }
         public static void main(String[] args) {
+            int LCM;
+            int HCF;
             OptimisedLCM lcm = new OptimisedLCM();
             Scanner scn = new Scanner(System.in);
             int val1 =  scn.nextInt();
             int val2 =  scn.nextInt();
-             lcm.HCF = lcm.getOptimisedHcf(val1,val2);
-            lcm.LCM = (val1*val2) / lcm.HCF;
+             HCF = lcm.getOptimisedHcf(val1,val2);
+            LCM = (val1*val2) / HCF;
 
 
             //a*b = hacf*lcm ---- 10,15 -- 10*15 = 5*30
 
-            System.out.println("LCM of "+val1 +" & "+ val2+"Will be :"+ lcm.LCM);
+            System.out.println("LCM of "+val1 +" & "+ val2+"Will be :"+ LCM);
         }
 }
