b6bc4fcab36046c94e31824af4d8ebf02547504d
RAMESH PANDEY <pandeyramesh400@gmail.com>
2022-08-19 19:25:03 +0530

    Check for Kth Set Bit

---
+++ src/com/PracticeJavaWithPandey/BitProblems/KthSetBit.java
@@ -0,0 +1,21 @@
+package com.PracticeJavaWithPandey.BitProblems;
+
+import java.util.Scanner;
+
+public class KthSetBit {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter Number");
+        int n = scn.nextInt();
+        System.out.println("Binary Representation of number will be :"+Integer.toBinaryString(n));
+        System.out.println("Enter Term you want to check");
+        int k = scn.nextInt();
+        if(((n>>(k-1))&1) == 1  ){
+            System.out.println("The "+k+"th Bit in Number "+n+" is "+ "SET i.e. -  1");
+        }
+        else{
+            System.out.println("The "+k+"th Bit in Number "+n+" is "+ "RESET i.e. -  0");
+        }
+    }
+}
