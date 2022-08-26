67107696ef43a2485beff54704cf1cc58a78a552
RAMESH PANDEY <pandeyramesh400@gmail.com>
2022-08-26 23:59:41 +0530

    Prime

---
+++ src/com/PracticeJavaWithPandey/MathsProblem/Prime/Prime.java
@@ -0,0 +1,38 @@
+package com.PracticeJavaWithPandey.MathsProblem.Prime;
+
+import java.util.Scanner;
+
+public class Prime {
+
+        boolean ISPRIME = true;
+        int n = 2;
+        boolean isPrime(int value){
+            if(value <= 1){
+                ISPRIME = false;
+            }
+            //check for every value starting from 2 if given value divided completly by n then it cannot be prime
+            while(n<value){
+                if(value%n==0){
+                    ISPRIME = false;
+                    break;
+                }
+                n++;
+            }
+            return ISPRIME;
+        }
+
+        public static void main(String[] args) {
+            com.PracticeJavaWithPandey.MathsProblem.Prime.BasicPrime bp = new com.PracticeJavaWithPandey.MathsProblem.Prime.BasicPrime();
+            Scanner scn = new Scanner(System.in);
+            int value  = scn.nextInt();
+            boolean result = bp.isPrime(value);
+            if(result){
+                System.out.println("PRIME!!!");
+            }
+            if(!result){
+                System.out.println("NON PRIME!!!");
+            }
+        }
+
+
+}
