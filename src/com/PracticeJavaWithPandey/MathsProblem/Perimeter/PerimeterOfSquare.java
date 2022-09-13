f6ed62dbc103dc68426957c758d71b60d220aabc
RAMESH PANDEY <pandeyramesh400@gmail.com>
2022-09-13 23:34:48 +0530

    Program for Perimeter of Square

---
+++ src/com/PracticeJavaWithPandey/MathsProblem/Perimeter/PerimeterOfSquare.java
@@ -0,0 +1,22 @@
+package com.PracticeJavaWithPandey.MathsProblem.Perimeter;
+
+import java.util.Scanner;
+
+public class PerimeterOfSquare {
+
+    static int getPerimeter(int side)
+    {
+        return (4*side);
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner scn = new Scanner(System.in);
+        int side = scn.nextInt();
+
+        int result  = getPerimeter(side);
+        System.out.println("The perimeter of the given Square of side "+ side + " = "
+                + result);
+    }
+
+}
