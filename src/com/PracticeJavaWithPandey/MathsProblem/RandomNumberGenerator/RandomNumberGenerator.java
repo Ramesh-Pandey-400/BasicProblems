bf7ecec28356d2d23182d1a03708698f9d741832
RAMESH PANDEY <pandeyramesh400@gmail.com>
2022-10-03 23:53:30 +0530

    Random Number Generator

---
+++ src/com/PracticeJavaWithPandey/MathsProblem/RandomNumberGenerator/RandomNumberGenerator.java
@@ -0,0 +1,7 @@
+package com.PracticeJavaWithPandey.MathsProblem.RandomNumberGenerator;
+
+public class RandomNumberGenerator {
+    public static void main(String[] args) {
+        System.out.println("Random Number Generated is :"+(int)(Math.random()*10));
+    }
+}
