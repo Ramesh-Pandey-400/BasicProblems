8b63767bb6aa71ed6c1640e10479c36b4d8acb33
RAMESH PANDEY <pandeyramesh400@gmail.com>
2022-10-19 23:06:51 +0530

    Program to get Ceil value of given value

---
+++ src/com/PracticeJavaWithPandey/MathsProblem/FloorAndCeil/CeilValuee.java
@@ -0,0 +1,15 @@
+package com.PracticeJavaWithPandey.MathsProblem.FloorAndCeil;
+
+import java.util.Scanner;
+
+public class CeilValuee {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Value");
+        double value  = scn.nextDouble();
+
+        double result = Math.ceil(value);
+        System.out.println("Ceil Value of  "+value+" is  "+result);
+
+    }
+}
