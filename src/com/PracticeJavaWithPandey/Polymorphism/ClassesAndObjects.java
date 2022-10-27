e7ce9103e5d7f6eb6f6a96221370ef6738df5c85
RAMESH PANDEY <pandeyramesh400@gmail.com>
2022-10-27 20:22:30 +0530

    Basic program to show classes and Objects.

---
+++ src/com/PracticeJavaWithPandey/Polymorphism/ClassesAndObjects.java
@@ -0,0 +1,17 @@
+package com.PracticeJavaWithPandey.Polymorphism;
+
+public class ClassesAndObjects {
+    public static void main(String[] args) {
+
+        //creating object of class SampleClass
+        SampleClass sc = new SampleClass();
+        sc.SampleFunction();
+    }
+}
+
+//creating sample class
+class SampleClass{
+    void SampleFunction(){
+        System.out.println("This is Hello World Program!");
+    }
+}
