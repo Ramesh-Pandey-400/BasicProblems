ec167e6266ee76925fcb3f8b04651e7648b9fdfb
RAMESH PANDEY <pandeyramesh400@gmail.com>
2022-10-29 21:50:36 +0530

    Simple Constructor in java

---
+++ src/com/PracticeJavaWithPandey/ObjectOrientedProgramming/Constructor/Constructor.java
@@ -0,0 +1,13 @@
+package com.PracticeJavaWithPandey.ObjectOrientedProgramming.Constructor;
+
+public class Constructor {
+
+        // Create a  constructor for the Constructor class
+        public Constructor() {
+            System.out.println("Constructor Called");
+        }
+
+        public static void main(String[] args) {
+            Constructor myObj = new Constructor();
+        }
+}
