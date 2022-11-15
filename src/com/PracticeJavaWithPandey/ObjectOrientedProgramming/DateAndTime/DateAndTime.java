fd928ca7796312dc483167f865cb432fa70c969b
RAMESH PANDEY <pandeyramesh400@gmail.com>
2022-11-15 23:32:14 +0530

    Date & Time in java

---
+++ src/com/PracticeJavaWithPandey/ObjectOrientedProgramming/DateAndTime/DateAndTime.java
@@ -0,0 +1,14 @@
+package com.PracticeJavaWithPandey.ObjectOrientedProgramming.DateAndTime;
+
+import java.time.LocalDate;
+
+public class DateAndTime {
+        public static void main(String[] args) {
+            LocalDate myObj = LocalDate.now();
+            System.out.println(myObj);
+        }
+
+
+
+
+}
