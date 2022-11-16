19fc09c61220a5084615718ebf81df1277ed3d01
RAMESH PANDEY <pandeyramesh400@gmail.com>
2022-11-16 19:29:00 +0530

    Local  time in java

---
+++ src/com/PracticeJavaWithPandey/ObjectOrientedProgramming/DateAndTime/TimeOnly.java
@@ -0,0 +1,10 @@
+package com.PracticeJavaWithPandey.ObjectOrientedProgramming.DateAndTime;
+
+import java.time.LocalTime;
+
+public class TimeOnly {
+        public static void main(String[] args) {
+            LocalTime myObj = LocalTime.now();
+            System.out.println(myObj);
+        }
+}
