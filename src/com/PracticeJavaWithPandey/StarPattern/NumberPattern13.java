2c8a05c7c1640a129eb86d44dc8c24222b698a1d
RAMESH PANDEY <pandeyramesh400@gmail.com>
2022-12-28 21:46:17 +0530

    Number Pattern 13 in java

---
+++ src/com/PracticeJavaWithPandey/StarPattern/NumberPattern13.java
@@ -0,0 +1,16 @@
+package com.PracticeJavaWithPandey.StarPattern;
+
+public class NumberPattern13 {
+    public static void main(String[] args)
+    {
+        int i, j, rows=5;
+        for (i = rows; i >= 1; i--)
+        {
+            for (j = 1; j <= i; j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
