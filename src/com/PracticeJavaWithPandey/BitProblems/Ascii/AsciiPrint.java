bf1aef54ff15eadca9a0851edc79481508199789
RAMESH PANDEY <pandeyramesh400@gmail.com>
2023-01-13 22:59:37 +0530

    Acii to char and vice versa

---
+++ src/com/PracticeJavaWithPandey/BitProblems/Ascii/AsciiPrint.java
@@ -0,0 +1,12 @@
+package com.PracticeJavaWithPandey.BitProblems.Ascii;
+
+import java.util.Scanner;
+
+public class AsciiPrint {
+    public static void main(String[] args)
+    {
+        char ch1 = 'a';
+        int asciivalue1 = ch1;
+        System.out.println("The ASCII value of " + ch1 + " is: " + asciivalue1);
+    }
+}
