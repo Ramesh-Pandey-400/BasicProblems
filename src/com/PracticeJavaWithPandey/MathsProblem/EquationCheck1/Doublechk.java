5e7abc013ab7ab4e5d189ccce24fb93126d7b420
RAMESH PANDEY <pandeyramesh400@gmail.com>
2023-01-17 23:59:24 +0530

    double chk

---
+++ src/com/PracticeJavaWithPandey/MathsProblem/EquationCheck1/Doublechk.java
@@ -0,0 +1,13 @@
+package com.PracticeJavaWithPandey.MathsProblem.EquationCheck1;
+
+public class Doublechk {
+        public static void main(String[] args) {
+            double d_num = 5.44444;
+            if ((d_num % 1) == 0) {
+                System.out.println("It's not a double number");
+            } else {
+                System.out.println("It's a double number");
+            }
+        }
+
+}
