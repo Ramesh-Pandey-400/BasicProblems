550b580f2c66577cabedb2915ebd1af11b249e71
RAMESH PANDEY <pandeyramesh400@gmail.com>
2023-01-20 23:49:38 +0530

    Double Part-Fraction and absolute value

---
+++ src/com/PracticeJavaWithPandey/MathsProblem/DoublePart/DoublePart.java
@@ -0,0 +1,14 @@
+package com.PracticeJavaWithPandey.MathsProblem.DoublePart;
+
+public class DoublePart {
+        public static void main(String[] args) {
+            double value = 12.56;
+            double fractional_part = value % 1;
+            double integral_part = value - fractional_part;
+            System.out.print("\nOriginal value: "+value);
+            System.out.print("\nIntegral part: "+integral_part);
+            System.out.print("\nFractional part: "+fractional_part);
+            System.out.println();
+        }
+
+}
