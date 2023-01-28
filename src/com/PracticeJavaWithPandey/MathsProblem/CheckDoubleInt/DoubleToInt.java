76540499af2d314a76c0f48551f66a8763f19779
RAMESH PANDEY <pandeyramesh400@gmail.com>
2023-01-28 23:59:02 +0530

    Double to int check in java

---
+++ src/com/PracticeJavaWithPandey/MathsProblem/CheckDoubleInt/DoubleToInt.java
@@ -0,0 +1,13 @@
+package com.PracticeJavaWithPandey.MathsProblem.CheckDoubleInt;
+
+public class DoubleToInt {
+        public static void main(String[] args) {
+            double d_num = 6.44444;
+            if ((d_num % 1) == 0) {
+                System.out.println("It's not a double number");
+            } else {
+                System.out.println("It's a double number");
+            }
+        }
+
+}
